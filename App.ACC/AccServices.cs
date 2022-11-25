cf3b57527f73d66c57158fdad6b89e33b2bfb240
terk182 <ittipon.tana.terk@gmail.com>
2022-11-26 00:57:48 +0700

    add caa code

---
+++ App.ACC/AccServices.cs
@@ -0,0 +1,19 @@
+﻿using narit_mis_api.AccModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ACC
+{
+    public class AccServices : IAccServices
+    {
+        private readonly NARIT_MIS_ACCContext _databaseACC;
+
+        public AccServices(NARIT_MIS_ACCContext context)
+        {
+            _databaseACC = context;
+        }
+    }
+}
---
+++ narit_mis_api/Controllers/AccountingController.cs
@@ -0,0 +1,29 @@
+﻿using App.ACC;
+using App.Common;
+using App.SEC;
+using Microsoft.AspNetCore.Mvc;
+
+namespace narit_mis_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountingController : Controller
+    {
+
+        protected readonly IAccServices _AccServices;
+        private readonly ILogger _Logger;
+
+        public AccountingController(ILogger<AccountingController> logger, IAccServices Service)
+        {
+            _AccServices = Service;
+            _Logger = logger;
+
+        }
+        [HttpGet]
+        [Route("Accounting/test")]
+        public IActionResult MainGeneralUserPage()
+        {
+            return Json("MainGeneralUserPage");
+        }
+    }
+}
