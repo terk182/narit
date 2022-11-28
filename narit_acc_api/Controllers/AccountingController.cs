e3b63eb7e03c1db7ea4ba20512bb903964bf54c7
terk182 <Ittipon.tana.terk@gmail.com>
2022-11-28 13:57:33 +0700

    add acc project

---
+++ narit_acc_api/Controllers/AccountingController.cs
@@ -0,0 +1,27 @@
+﻿using App.ACC;
+using Microsoft.AspNetCore.Mvc;
+
+namespace narit_acc_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountingController : Controller
+    {
+        protected readonly IAccServices _AccServices;
+        private readonly ILogger _Logger;
+
+        public AccountingController(ILogger<AccountingController> logger, IAccServices Service)
+        {
+            _AccServices = Service;
+            _Logger = logger;
+
+        }
+        [HttpGet]
+        [Route("Accounting/test")]
+        public IActionResult MainGeneralUserPage()
+        {
+            var data = _AccServices.GetAccbooks();
+            return Json(data);
+        }
+    }
+}
--- narit_mis_api/Program.cs
+++ narit_mis_api/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //var r = new DependencyResolver();
 builder.Services.AddCustomServices();
-
+builder.Services.AddAccServices();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.PropertyNamingPolicy = null;
