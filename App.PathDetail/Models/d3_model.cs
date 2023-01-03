f4939893ed5c882b1bf6154695d5fb7bf90e8f3b
terk182 <ittipon.tana.terk@gmail.com>
2023-01-03 10:03:11 +0700

    add

---
+++ App.PathDetail/Models/d3_model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.PathDetail.Models
+{
+    public class d3_model
+    {
+        public string? name { get; set; }
+        public List<Child>? children { get; set; }
+        public class Child
+        {
+            public string? name { get; set; }
+            public List<Child>? children { get; set; }
+            public string? type { get; set; }
+        }
+
+    }
+}
--- ex_web/Controllers/HomeController.cs
+++ ex_web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using App.PathDetail;
+using App.PathDetail.Models;
 using ex_web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using static App.PathDetail.Models.d3_model;
 
 namespace ex_web.Controllers
 {
@@ -9,24 +11,27 @@ namespace ex_web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         protected readonly IPathDetailService _Service;
+        protected readonly List<db_list> _db_class;
+        protected readonly List<backlist> _backlist;
         public HomeController(ILogger<HomeController> logger, IPathDetailService Service)
         {
             _Service = Service;
             _logger = logger;
+            _db_class = _Service.get_db_class("./json/database_class_list.json");
+            _backlist = _Service.get_backlist("./json/backlist.json");
         }
 
         public IActionResult Index()
         {
-            var data = _Service.get_db_class("./json/database_class_list.json");
-            ViewBag.db = data;
+            ViewBag.db = _db_class;
             return View();
         }
         public IActionResult relation(string table)
         {
 
             var sub_table = table;
-            var data = _Service.get_db_class("./json/database_class_list.json");
-            var get_backlist = _Service.get_backlist("./json/backlist.json");
+            var data = _db_class;
+            var get_backlist = _backlist;
             bool check = false;
             foreach (var backlist in get_backlist)
             {
@@ -91,7 +96,59 @@ namespace ex_web.Controllers
         {
             return View();
         }
+        public IActionResult Tree(string table)
+        {
+            var main = new d3_model();
+            var list = new List<Child>();
+            var data = _db_class.Where(x => x.table == table).ToList();
+            if (data.Count != 0)
+            {
+                main.name = table;
+                foreach (var item in data[0].data)
+                {
+                    if (item.type == "virtual")
+                    {
+                        var sub1 = _db_class.Where(x => x.table == item.field).ToList();
 
+                        if (sub1.Count != 0)
+                        {
+
+                            var list_sub1 = new List<Child>();
+                            foreach (var item1 in sub1[0].data)
+                            {
+                                if (item1.type == "virtual")
+                                {
+
+                                    list_sub1.Add(new Child
+                                    {
+                                        name = item1.field,
+                                        type = item1.type
+                                    });
+                                }
+
+                            }
+
+
+                            list.Add(new Child
+                            {
+                                name = item.field,
+                                type = item.type,
+                                children = list_sub1
+
+                            });
+                        }
+                    }
+
+                }
+                main.children = list;
+            }
+
+            ViewBag.data = main;
+            return View();
+        }
+
+
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
