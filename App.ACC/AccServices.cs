718952be3d3428d6c6ad1c985e1337a7c4231b37
poonsab <poonsab26375@gmail.com>
2023-01-05 11:31:11 +0700

    update model acc

--- App.ACC/AccServices.cs
+++ App.ACC/AccServices.cs
@@ -1,4 +1,4 @@
-﻿using narit_mis_api.AccModels;
+﻿using narit_acc_api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +16,9 @@ namespace App.ACC
             _databaseACC = context;
         }
 
-        public List<Accbook> GetAccbooks()
+        public List<ChartAcc> GetAccbooks()
         {
-            var data = _databaseACC.Accbooks.ToList();
+            var data = _databaseACC.ChartAccs.ToList();
             return data;
         }
     }
