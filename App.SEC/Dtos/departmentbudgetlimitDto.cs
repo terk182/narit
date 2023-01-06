f45c0207f418373d0ca8076a3f50bea760f9e911
pakpoom rujipan <pakpoomruj@gmail.com>
2023-01-06 10:04:57 +0700

    /
    /

---
+++ App.SEC/Dtos/departmentbudgetlimitDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.SEC.Dtos
+{
+    public class departmentbudgetlimitDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int FiscalYear { get; set; }
+        public bool Active { get; set; }
+        public int? ParentDepartmentId { get; set; }
+        public int? ReferenceOldId { get; set; }
+        public int? BudgetLimit { get; set; }
+    }
+}
