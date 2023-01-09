1019755df0b43d4117bb350ae787eeb0b8e2c4ad
poonsab <poonsab26375@gmail.com>
2023-01-09 15:31:58 +0700

    config file GIT

---
+++ App.AccountsReceivable/AccountsReceivableService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using narit_acc_api.Models;
+
+namespace App.AccountsReceivable
+{
+    public class AccountsReceivableService : IAccountsReceivableService
+    {
+        private readonly CGI_ACCContext _databaseACC;
+
+        public AccountsReceivableService(CGI_ACCContext context)
+        {
+            _databaseACC = context;
+        }
+
+
+        public List<ChartHeader> GetChartHeaders()
+        {
+            var data = _databaseACC.ChartHeaders.ToList();
+            if (data.Count > 0)
+            {
+                return data;
+            }
+            return data;
+        }
+    }
+}
---
+++ App.AccountsReceivable/IAccountsReceivableService.cs
@@ -0,0 +1,14 @@
+﻿using narit_acc_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.AccountsReceivable
+{
+    public interface IAccountsReceivableService
+    {
+        List<ChartHeader> GetChartHeaders();
+    }
+}
