6ad8133cea38a56ce21adb85e87bf4090a0267c2
terk182 <ittipon.tana.terk@gmail.com>
2023-01-24 14:22:18 +0700

    add setupcontroller

---
+++ narit_mis_api/Controllers/SetUpController.cs
@@ -0,0 +1,23 @@
+﻿using App.PathDetail;
+using App.Plan;
+using App.SEC;
+using Microsoft.AspNetCore.Mvc;
+
+namespace narit_mis_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SetUpController : Controller
+    {
+        protected readonly IPathDetailService _Service;
+        private readonly ILogger _Logger;
+        public SetUpController(ILogger<SiteDetailController> logger, IPathDetailService Service)
+        {
+            _Service = Service;
+            _Logger = logger;
+
+            //_configuration = Configuration;
+            // connecttion = _configuration.GetConnectionString("Defaultconnectton");
+        }
+    }
+}
