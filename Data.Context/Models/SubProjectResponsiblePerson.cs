fe88b5e71a7f2472828ec5374dd45ea4286ab575
pakpoom rujipan <pakpoomruj@gmail.com>
2023-02-12 14:55:58 +0700

    add position in responsibleperson

--- Data.Context/Models/SubProjectResponsiblePerson.cs
+++ Data.Context/Models/SubProjectResponsiblePerson.cs
@@ -9,5 +9,6 @@ namespace narit_mis_api.Models
         public int StaffId { get; set; }
         public int SubProjectId { get; set; }
         public bool Active { get; set; }
+        public string Position { get; set; } = null!;
     }
 }
